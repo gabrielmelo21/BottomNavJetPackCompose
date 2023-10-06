11c99565ed3d80f1f38f27eb6f9f4fd33566fd02
Gabriel de Melo <89091392+gabrielmelo21@users.noreply.github.com>
2023-10-05 21:22:48 -0300

    init
    init

--- app/src/main/java/com/melo/bottomnavjetpackcompose/MainScreen.kt
+++ app/src/main/java/com/melo/bottomnavjetpackcompose/MainScreen.kt
@@ -1,16 +1,21 @@
 package com.melo.bottomnavjetpackcompose
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement.Bottom
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentColor
 import androidx.compose.material.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
@@ -60,11 +65,16 @@ BottomNavigationItem(
   Text(text = screen.title)
  },
  icon = {
-    Icon(imageVector = screen.icon, contentDescription = "Navigation Icon" )
+    Icon(
+        imageVector = screen.icon,
+        contentDescription = "Navigation Icon",
+      )
  },
  selected = currentDestination?.hierarchy?.any{
   it.route == screen.route
  } == true,
+    unselectedContentColor = LocalContentColor.current.copy(alpha = ContentAlpha.disabled),
+
  onClick = {
   navController.navigate(screen.route)
  }
